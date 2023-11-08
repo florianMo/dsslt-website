ba8ebc8f05481d6da19fffc745d208a9489d1a1a
Florian Motteau <florian.motteau@nereide.fr>
2023-11-08 20:29:48 +0100

    start ptyx section

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,43 @@
-<h1 class="title">Des sons sous la Terre </h1>
+<script>
+  import ptyxImage from "$lib/images/ptyx.jpeg";
+</script>
+
+<h1 class="title">Des sons sous la Terre</h1>
 <p>Lieu modeste et souterrain d'expression musicale</p>
+
+<div class="container">
+  <section style="height: 514px">
+    <div class="left">
+      <span class="date">18/09/2023</span>
+      <h2>Ensemble PTYX</h2>
+      <div class="description">
+        <p>
+          PTYX est un ensemble de musiciens qui se consacrent à l’exploration des musiques de création
+          sous toutes ses formes.
+        </p>
+        <p>
+          Basé en Indre-et-Loire, le collectif multiplie les formes et les mises en scène pour aller à
+          la rencontre de tous les publics.
+        </p>
+        <h3>Lugasban</h3>
+        <span class="composer">Krisztina Megyeri</span>
+        <p>Pauline Vanagt, Benjamin Garnier</p>
+        <h3>Petals</h3>
+        <span class="composer">Kaija Saariaho</span>
+        <p>Benjamin Garnier, Jean-Baptiste Apéré</p>
+        <h3>Pression</h3>
+        <span class="composer">Helmut Lachenmann</span>
+        <p>Benjamin Garnier</p>
+        <h3>Cello counterpoint</h3>
+        <span class="composer">Steve Reich</span>
+        <p>Benjamin Garnier</p>
+      </div>
+      <div class="links">
+        <a href="https://www.ensembleptyx.com/" target="_blank">PTYX</a>
+      </div>
+    </div>
+    <div class="right">
+      <img src="{ptyxImage}" alt="ptyx" />
+    </div>
+  </section>
+</div>
